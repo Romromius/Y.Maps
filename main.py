92b87caa374c23a43c1a851c24ae1dfdfb90c2a2
HP <635roman.r642@gmail.com>
2024-02-06 15:30:41 +1000

    task2

--- main.py
+++ main.py
@@ -1,35 +1,86 @@
+import json
 import os
+import random
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton, QLabel
 import requests
+from PyQt5 import QtCore
 from PyQt5.QtGui import QPixmap
+from speaker import Speaker
 
 
 class Map(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.speaker = Speaker()
         self.setWindowTitle("картка")
-        self.setGeometry(350, 350, 600, 600)
+        self.setFixedSize(600, 520)
 
-        ZAPROS = "Владивосток"
-        geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={ZAPROS}&format=json"
-        response = requests.get(geocoder_request)
-        json_response = response.json()
-        coords = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"][
-            "pos"].split()
+        self.img = QLabel(self)
+        self.img.setText('Загрузка/Введите новый запрос')
+        self.img.setAlignment(QtCore.Qt.AlignCenter)
+        self.img.resize(600, 600)
 
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords[0]},{coords[1]}&spn=30,30&l=map"
-        response = requests.get(map_request)
+        self.point = None
+
+        self.map_spn = 1
+        self.map_file = None
+
+        self.search("vladivostok")
+
+    def search(self, toponym):
+        try:
+            geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={toponym}&format=json"
+            response = requests.get(geocoder_request)
+            json_response = response.json()
+            self.coords = [float(i) for i in json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"].split()]
+
+        except IndexError:
+            self.speaker.say('bad beep', 'error', 'search error', important=True)
+            self.img.setText('Ошибка поиска!')
+            return
+        except requests.exceptions.ConnectionError:
+            self.speaker.say('bad beep', 'error', 'no net', important=True)
+            self.img.setText('Нет сети!')
+            return
+        self.map_file = "map.png"
+        self.speaker.say('neutral beep', 'request get')
+
+        self.update_map()
+
+    def update_map(self):
+        try:
+            response = requests.get(f'http://static-maps.yandex.ru/1.x/?ll={self.coords[0]},{self.coords[1]}&l=map&z={self.map_spn}')
+        except requests.exceptions.ConnectionError:
+            self.speaker.say('bad beep', 'error', 'no net', important=True)
+            self.img.setText('Нет сети!')
+            return
         if not response:
             sys.exit(1)
-        map_file = "map.png"
-        with open(map_file, "wb") as file:
+        with open('map.png', "wb") as file:
             file.write(response.content)
+        self.img.setPixmap(QPixmap(self.map_file))
 
-        self.pix = QPixmap(map_file)
-        self.img = QLabel(self)
-        self.img.resize(600, 600)
-        self.img.setPixmap(self.pix)
+    def keyPressEvent(self, event):
+        match event.key():
+            case 45:
+                self.map_spn -= 1
+            case 61:
+                self.map_spn += 1
+            case _:
+                return
+
+        if self.map_spn <= 0:
+            self.speaker.say('bad beep')
+            self.map_spn = 1
+            return
+        elif self.map_spn >= 22:
+            self.speaker.say('bad beep')
+            self.map_spn = 21
+            return
+
+        self.update_map()
+        print(self.coords)
 
 
 if __name__ == '__main__':
